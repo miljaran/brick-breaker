700d1d7765245b7ac56bd6f58d4a5e9ff41ff358
Milja Rantanen <milja.rantanen@gmail.com>
2024-03-05 16:01:59 +0200

    ability to move the paddle

--- src/brickBreaker/GamePanel.java
+++ src/brickBreaker/GamePanel.java
@@ -2,21 +2,45 @@ package brickBreaker;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
     private final Paddle paddle;
 
     public GamePanel() {
         setPreferredSize(new Dimension(BrickBreakerGame.WIDTH, BrickBreakerGame.HEIGHT));
         setBackground(Color.BLACK);
         setFocusable(true);
+        addKeyListener(this);
 
         paddle = new Paddle((BrickBreakerGame.WIDTH - Paddle.WIDTH) / 2, BrickBreakerGame.HEIGHT - 2 * Paddle.HEIGHT);
+
+        Timer timer = new Timer(10, this);
+        timer.start();
     }
 
-        @Override
+    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         paddle.draw(g);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        paddle.move();
+        repaint();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        paddle.keyPressed(e);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        paddle.keyReleased(e);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
 }
--- src/brickBreaker/Paddle.java
+++ src/brickBreaker/Paddle.java
@@ -1,17 +1,48 @@
 package brickBreaker;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Paddle {
     public static final int WIDTH = 100;
     public static final int HEIGHT = 20;
+    private static final int SPEED = 5;
 
-    private final int x;
+    private int x;
     private final int y;
+
+    private boolean leftPressed, rightPressed;
 
     public Paddle(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    // TODO: add limits to the sides
+    public void move() {
+        if (leftPressed) {
+            x -= SPEED;
+        } else if (rightPressed) {
+            x += SPEED;
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_LEFT) {
+            leftPressed = true;
+        } else if (key == KeyEvent.VK_RIGHT) {
+            rightPressed = true;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_LEFT) {
+            leftPressed = false;
+        } else if (key == KeyEvent.VK_RIGHT) {
+            rightPressed = false;
+        }
     }
 
     public void draw(Graphics g) {
